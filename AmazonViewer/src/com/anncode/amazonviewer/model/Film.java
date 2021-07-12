b835b52854663d807d710dbd665e5be6d73af7e6
Teh Zeo <86634981+tehzeo@users.noreply.github.com>
2021-07-12 08:39:24 -0700

    2.2 Re Uploading good files (trying too!)
    Theres bad internet files are getting wrong!

---
+++ AmazonViewer/src/com/anncode/amazonviewer/model/Film.java
@@ -0,0 +1,98 @@
+package com.anncode.amazonviewer.model;
+
+
+/**
+ * <h1>Film</h1>
+ * Film is an abstract father class.
+ * <p>
+ * This class is the class base from the Films family class, being abstract
+ * you are not allowed to instance. It contains an abstract method.
+ * {@code view()} it is obligatory in every member of the family.
+ * 
+ * 
+ * @author Jose Roberto Urrutia L.
+ * @version 1.1
+ * @since 2021
+ * */
+public abstract class Film {
+	
+	private String title;
+	private String genre;
+	private String creator;
+	private int duration;
+	private short year;
+	private boolean viewed;
+	
+	
+	public Film() {}
+	public Film(String title, String genre, String creator, int duration) {
+		super();
+		this.title = title;
+		this.genre = genre;
+		this.creator = creator;
+		this.duration = duration;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getCreator() {
+		return creator;
+	}
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+	public int getDuration() {
+		return duration;
+	}
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	public short getYear() {
+		return year;
+	}
+	public void setYear(short year) {
+		this.year = year;
+	}
+	public String isViewed() {
+		String visto = "";
+		if(viewed == true) {
+			visto = "Sí";
+		}else {
+			visto = "No";
+		}
+		
+		return visto;
+	}
+	
+	public boolean getIsViewed() {
+		return viewed;
+	}
+	public void setViewed(boolean viewed) {
+		this.viewed = viewed;
+	}
+	
+	/**
+	 * {@code view()} its obligatory to implement!!
+	 * */
+	public abstract void view();
+
+}
+
+
+
+
+
+
+
+
+
